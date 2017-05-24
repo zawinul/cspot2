c9b26f29a64231401fe7a041d2a93debafb131e8
Paolo Andrenacci <paolo.andrenacci@gmail.com>
2017-05-24 12:04:17 +0200

    aaa bbb ccc

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 module.exports = function (grunt) {
     // Project configuration.
     grunt.initConfig({
